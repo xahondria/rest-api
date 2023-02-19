51563956b3e8cb54816ef949b5d564f7fbc9caf7
Kolomoyets <kolomoyetsva@gmail.com>
2023-02-19 09:33:55 +0400

    How To Add Hot-Reload To Our Typescript Development Environment

--- src/routes/root.ts
+++ src/routes/root.ts
@@ -3,4 +3,5 @@ import { Request, Response } from 'express';
 export function root(request: Request, response: Response) {
 
   response.status(200).send('<h1>Express server is up and running.</h1>');
+
 }
