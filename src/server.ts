ce6a4107862c34a87c62e7949d000e7af07e58a6
Kolomoyets <kolomoyetsva@gmail.com>
2023-02-19 09:05:06 +0400

    Setting Up An Express Server From Scratch

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,14 @@
-console.log('hello world ')
+import * as express from 'express';
+
+
+const app = express();
+
+function setupExpress() {
+  app.route('/').get(root);
+}
+
+function startServer() {
+}
+
+setupExpress();
+startServer();
