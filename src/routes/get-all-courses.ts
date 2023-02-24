b33ed8d8e3a1c62f45e8d18fe89472e66de0c8f9
Kolomoyets <kolomoyetsva@gmail.com>
2023-02-24 10:02:01 +0400

    Error Handling In Asynchronous Express Endpoints

--- src/routes/get-all-courses.ts
+++ src/routes/get-all-courses.ts
@@ -1,17 +1,22 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import { logger } from '../logger';
 import { AppDataSource } from '../data-source';
 import { Course } from '../models/course';
 
-export async function getAllCourses(request: Request, response: Response) {
+export async function getAllCourses(request: Request, response: Response, next: NextFunction) {
 
-  logger.debug('Called getAllCourses()');
+  try {
+    logger.debug('Called getAllCourses()');
 
-  const courses = await AppDataSource.getRepository(Course)
-    .createQueryBuilder('courses')
-    .orderBy('courses.seqNo')
-    .getMany();
+    const courses = await AppDataSource.getRepository(Course)
+      .createQueryBuilder('courses')
+      .orderBy('courses.seqNo')
+      .getMany();
 
-  response.status(200).json({ courses });
+    response.status(200).json({ courses });
+  } catch (error) {
+    logger.error('Error calling getAllCourses()');
+    return next(error);
+  }
 
 }
