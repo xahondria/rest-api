26375f00562c5295933ead808de20899d0e7bd91
Kolomoyets <kolomoyetsva@gmail.com>
2023-02-24 08:39:11 +0400

    Populating a Database With TypeORM - Repositories

--- src/models/populate-db.ts
+++ src/models/populate-db.ts
@@ -6,12 +6,47 @@ import 'reflect-metadata';
 
 import { COURSES } from './db-data';
 import { AppDataSource } from '../data-source';
+import { Course } from './course';
+import { DeepPartial } from 'typeorm';
+import { Lesson } from './lesson';
 
 async function populateDb() {
 
   await AppDataSource.initialize();
   console.log('Database connection ready.');
 
+  const courses = Object.values(COURSES) as Array<DeepPartial<Course>>;
+
+  const courseRepository = AppDataSource.getRepository(Course);
+  const lessonsRepository = AppDataSource.getRepository(Lesson);
+
+  for (let courseData of courses) {
+
+    console.log(`Inserting course ${ courseData.title }`);
+
+    const course = courseRepository.create(courseData);
+
+    await courseRepository.save(course);
+
+    for (let lessonData of courseData.lessons) {
+      console.log(`Inserting lesson ${ lessonData.title }`);
+
+      const lesson = lessonsRepository.create(lessonData);
+      lesson.course = course;
+
+      await lessonsRepository.save(lesson);
+    }
+  }
+
+  const totalCourses = await courseRepository
+    .createQueryBuilder()
+    .getCount();
+
+  const totalLessons = await lessonsRepository
+    .createQueryBuilder()
+    .getCount();
+
+  console.log(` Data inserted - courses ${ totalCourses }, lessons ${ totalLessons }`);
 }
 
 populateDb()
