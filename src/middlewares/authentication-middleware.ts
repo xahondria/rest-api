544db615cfc1a004875d063cd926f493da034287
Kolomoyets <kolomoyetsva@gmail.com>
2023-02-28 00:53:52 +0400

    Implementation of an Authentication Middleware

--- src/middlewares/authentication-middleware.ts
+++ src/middlewares/authentication-middleware.ts
@@ -1,7 +1,43 @@
 import { NextFunction, Request, Response } from 'express';
+import { logger } from '../logger';
+
+const JWT_SECRET = process.env.JWT_SECRET;
+const jwt = require('jsonwebtoken');
 
 export function checkIfAuthenticated(
   request: Request, response: Response, next: NextFunction
 ) {
 
+  const authJwtToken = request.headers.authorization;
+
+  if (!authJwtToken) {
+    logger.info('The authentication JWT is not present, access denied.');
+    response.sendStatus(403);
+    return;
+  }
+
+  checkJwtValidity(authJwtToken)
+    .then(user => {
+
+      logger.info('Authentication JWT successfully decoded:', user);
+
+      // save user object for next handlers
+      request['user'] = user;
+
+      next();
+    })
+    .catch(err => {
+      logger.error(`Could not validate the authentication JWT, access denied.`, err);
+      response.sendStatus(403);
+    });
+
+}
+
+async function checkJwtValidity(authJwtToken: string) {
+
+  const user = jwt.verify(authJwtToken, JWT_SECRET);
+
+  logger.info('Found user details in JWT: ', user);
+
+  return user;
 }
