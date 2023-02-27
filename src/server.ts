adba9bd353f1928139ed98acf8d9a43cab732853
Kolomoyets <kolomoyetsva@gmail.com>
2023-02-28 01:10:57 +0400

    Role Based Authorization Using an Express Middleware

--- src/server.ts
+++ src/server.ts
@@ -23,6 +23,7 @@ import { deleteCourseAndLessons } from './routes/delete-course';
 import { createUser } from './routes/create-user';
 import { login } from './routes/login';
 import { checkIfAuthenticated } from './middlewares/authentication-middleware';
+import { checkIfAdmin } from './middlewares/admin-only.middleware';
 
 const cors = require('cors');
 const bodyParser = require('body-parser');
@@ -42,7 +43,7 @@ function setupExpress() {
   app.route('/api/courses/:courseId').patch(checkIfAuthenticated, updateCourse);
   app.route('/api/courses').post(checkIfAuthenticated, createCourse);
   app.route('/api/courses/:courseId').delete(checkIfAuthenticated, deleteCourseAndLessons);
-  app.route('/api/users').post(checkIfAuthenticated, createUser);
+  app.route('/api/users').post(checkIfAuthenticated, checkIfAdmin, createUser);
   app.route('/api/login').post(login);
 
   app.use(defaultErrorHandler);
